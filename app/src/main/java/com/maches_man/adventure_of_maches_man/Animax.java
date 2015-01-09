f488b0e680e42ced9e0f0a6789b5fcd3ee0d8ac7
michael liu <te0919193534@gmail.com>
2015-01-09 15:56:58 +0800

    enemy finish

--- app/src/main/java/com/maches_man/adventure_of_maches_man/Animax.java
+++ app/src/main/java/com/maches_man/adventure_of_maches_man/Animax.java
@@ -3,6 +3,7 @@ package com.maches_man.adventure_of_maches_man;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.Log;
 
 public class Animax {
 
@@ -25,18 +26,7 @@ public class Animax {
         this.pic=pic;
         this.pic_number=this.pic.length;
     }
-    /*public void setDuration(int duration){//設定長度
-        this.duration=duration;
-        this.count_unit=(pic_number*1.0)/(this.duration*1.0);
-    }*/
-    /*public void setPosition(int x,int y){//設定位置
-        this.x=x;
-        this.y=y;
-    }*/
-    /*public void start(int CurrentPosition){//啟動(有設定長度)
-        this.start_position=CurrentPosition;
-        animax_flag=true;
-    }*/
+
     public void start(){//啟動(無設定長度)
         animax_flag=true;
         animax_count_flag=0;
@@ -57,16 +47,7 @@ public class Animax {
     public int getCount(){
         return (int)animax_count_flag;
     }
-    /*public void drawEffect_time(int CurrentPosition,Canvas canvas,Paint paint){//繪圖(有設定長度){
-        if(animax_flag){
-            animax_count_flag=count_unit*(CurrentPosition-start_position);
-            if(((int)animax_count_flag)<pic_number){
-                Graphic.drawPic(canvas, pic[((int)animax_count_flag)], x, y, 0, 255, paint);
-            }else{
-                animax_flag=false;
-            }
-        }
-    }*/
+
     public void drawEffect(double speed,Canvas canvas,Paint paint,int x,int y,int face_flag){//繪圖(無設定長度)
         if(animax_flag){
             if(!pause_flag){
@@ -94,12 +75,5 @@ public class Animax {
         return animax_flag;
     }
 
-    //public int getCountFlag(){
-    //return ((int)animax_count_flag);
-    //}
-    /*public void recycle(){
-        for(int i=0;i<pic.length;i++){
-            pic[i].recycle();
-        }
-    }*/
+
 }
